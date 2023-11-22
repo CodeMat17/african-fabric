82921a3bd735f4a9a619ef962c9d114aaef929fb
CodeMat <codemat.biz@gmail.com>
2023-11-22 02:02:09 +0100

    updated

--- components/CustomerCard.js
+++ components/CustomerCard.js
@@ -1,5 +1,3 @@
-import { CldImage } from "next-cloudinary";
-import Image from "next/image";
 import Link from "next/link";
 import RecentOrderImageComponent from "./RecentOrderImageComponent";
 
@@ -8,34 +6,38 @@ const CustomerCard = ({ id, avatar, name, email, style, fabric }) => {
     <Link href={`/dashboard/orders/${id}`}>
       <div className='text-sm font-light border p-3 rounded-xl'>
         <div className='flex items-center justify-between'>
-          <div className=' flex items-center'>
+          <div className='flex items-center  w-[90%] sm:w-[70%]'>
             <RecentOrderImageComponent
               image={avatar}
               width='48'
               height='48'
               radius='rounded-full'
             />
-            <div className='max-w-[150px] sm:max-w-full md:max-w-[150px] lg:max-w-full  leading-4 ml-2'>
+            <div className='max-w-[150px] sm:max-w-full md:max-w-[150px] lg:max-w-full leading-4 ml-2'>
               <p className='text-[#55c694] font-medium truncate'>{name}</p>
               <p className='text-gray-500 truncate'>{email}</p>
+              <p className='sm:hidden pt-1 truncate uppercase'>&bull; {style}</p>
             </div>
           </div>
-          <div className='flex flex-col sm:flex-row gap-1 sm:gap-2 items-end sm:items-center justify-between'>
-            <p className='whitespace-nowrap capitalize truncate  max-w-[95px]'>
+          <div className='flex items-center gap-4'>
+            <p className='hidden sm:flex capitalize text-right'>
               {style}
             </p>
-            <RecentOrderImageComponent
-              image={fabric}
-              width='50'
-              height='30'
-              radius='rounded-lg sm:hidden'
-            />
-            <RecentOrderImageComponent
+            <div className='flex justify-end'>
+              <RecentOrderImageComponent
+                image={fabric}
+                width='50'
+                height='30'
+                radius='rounded-lg sm:hidden'
+              />
+               <RecentOrderImageComponent
               image={fabric}
               width='65'
               height='40'
               radius='rounded-lg hidden sm:block'
             />
+            </div>
+           
           </div>
         </div>
       </div>
