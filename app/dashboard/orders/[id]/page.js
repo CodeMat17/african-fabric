bf8d62e0462c4bd212a473d58f22f7a1de00110e
CodeMat <codemat.biz@gmail.com>
2023-11-22 02:58:07 +0100

    updated

--- app/dashboard/orders/[id]/page.js
+++ app/dashboard/orders/[id]/page.js
@@ -37,7 +37,7 @@ const OrderDetailsPage = async ({ params: { id } }) => {
           <p className='text-gray-500'>{customer.email}</p>
           <p className='text-gray-500'>{customer.tel}</p>
           <div className='flex divide-x-2 divide-gray-300 gap-4 text-sm text-gray-400 mt-2'>
-            <p class>{customer.style}</p>
+            <p className="capitalize"> {customer.style}</p>
             <p className='pl-4 whitespace-nowrap'>
               booked on {dayjs(customer.created_at).format("MMM DD, YYYY")}{" "}
             </p>
