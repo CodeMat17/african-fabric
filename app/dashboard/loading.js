78d686338add345ccfb43cb392e32d25f072b7d1
CodeMat <codemat.biz@gmail.com>
2023-12-06 12:17:20 +0100

    updated

--- app/dashboard/loading.js
+++ app/dashboard/loading.js
@@ -4,7 +4,7 @@ import {Spinner} from "@nextui-org/react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center pt-20">
+    <div className="flex justify-center pt-20 whitespace-nowrap">
       <Spinner label='Please wait...' color='success' labelColor='success' />
     </div>
   );
